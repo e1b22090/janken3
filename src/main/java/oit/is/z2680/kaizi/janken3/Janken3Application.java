debb090e666d7f614ad5ae353fe5c6ba7d8c7ae5
e1b22078 <e1b22078@oit.ac.jp>
2024-10-01 14:42:56 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z2680/kaizi/janken3/Janken3Application.java
@@ -0,0 +1,13 @@
+package oit.is.z2680.kaizi.janken3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken3Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z2680/kaizi/janken3/Janken3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z2680.kaizi.janken3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
